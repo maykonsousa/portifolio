ae3b7051cf4f8f9f817c591064e32225d3d6fbd0
Maykon Sousa <maykon.sousa@hotmail.com>
2023-01-29 15:56:45 -0300

    fix: ajusta responsividade do techButton

--- src/components/TechButton/TechButton.styles.ts
+++ src/components/TechButton/TechButton.styles.ts
@@ -9,6 +9,18 @@ export const TechButtonContainer = styled.li`
   width: 7rem;
   font-weight: bold;
 
+  //mobile
+  @media (max-width: 768px) {
+    width: 6rem;
+    font-size: 75%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      display: none;
+    }
+  }
+
   &:hover {
     background: ${({ theme }) => theme.colors["purple-500"]};
   }
